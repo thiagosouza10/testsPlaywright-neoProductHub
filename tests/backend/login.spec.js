ddec1cfcabe48b35fbc74084cf9bac0ec67fa623
Thiago de Souza <thiagodesouza22@gmail.com>
2026-04-16 18:52:32 -0300

    melhorias

--- tests/backend/login.spec.js
+++ tests/backend/login.spec.js
@@ -3,15 +3,16 @@ import { test, expect } from '@playwright/test';
 import { endpointsLogin } from '../../utils/paths/login';
 
 test.describe('Backend - Login', () => {
-  test('Deve efetuar login com sucesso', async ({ request }) => {
+  test('Deve efetuar login com sucesso', async ({ request }, testInfo) => {
     const response = await request.post(endpointsLogin.pathLogin(), {
       data: {
         username: "admin",
         password: process.env.SENHA
       }
     });
-    expect(response.status()).toBe(200);
     const body = await response.json();
+    await testInfo.attach(`Response POST Login:`, { body: JSON.stringify(body, null, 2) });
+    expect(response.status()).toBe(200);
     expect(body.message).toBe("Login realizado com sucesso");
   });
 });  
