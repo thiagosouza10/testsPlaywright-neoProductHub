5f4c1450ed9b55e768dd12ffefd5da03dbfc8d66
Thiago de Souza <thiagodesouza22@gmail.com>
2026-04-16 21:40:29 -0300

    melhorua validação do teste de login api

--- tests/backend/login.spec.js
+++ tests/backend/login.spec.js
@@ -2,6 +2,8 @@
 import { test, expect } from '@playwright/test';
 import { endpointsLogin } from '../../utils/paths/login';
 
+const dataRegex = /^\d{4}-\d{2}-\d{2}T/
+
 test.describe('Backend - Login', () => {
   test('Deve efetuar login com sucesso', async ({ request }, testInfo) => {
     const response = await request.post(endpointsLogin.pathLogin(), {
@@ -13,6 +15,18 @@ test.describe('Backend - Login', () => {
     const body = await response.json();
     await testInfo.attach(`Response POST Login:`, { body: JSON.stringify(body, null, 2) });
     expect(response.status()).toBe(200);
-    expect(body.message).toBe("Login realizado com sucesso");
+    expect(body).toEqual(expect.objectContaining({
+      success: true,
+      message: "Login realizado com sucesso",
+      user: {
+        id: "1",
+        name: "Administrador",
+        username: "admin",
+        role: "admin",
+        active: true,
+        createdAt: expect.stringMatching(dataRegex),
+        lastLogin: expect.stringMatching(dataRegex)
+      }
+    }))
   });
 });  
